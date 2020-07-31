b7b70b79e82480455c813164ee2bbe884052380c
SnowballBoy <54330340+SnowballBoy@users.noreply.github.com>
2020-07-31 15:28:48 +0300

    Добавляет шапку сайта

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,21 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var header = document.querySelector('.header');
+var headerTopWrapper = header.querySelector('.header__top-wrapper');
+var headerNav = header.querySelector('.nav');
+var headerBurger = header.querySelector('.header__burger');
+var headerLogo = header.querySelector('.header__logo-img');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+header.classList.remove('header--nojs');
+headerTopWrapper.classList.remove('header__top-wrapper--nojs');
+headerNav.classList.remove('nav--nojs');
+
+headerLogo.classList.add('header__logo-img--light');
+
+function menuToggle() {
+  headerNav.classList.toggle('nav--opened');
+  headerBurger.classList.toggle('header__burger--opened');
+  headerLogo.classList.toggle('header__logo-img--light');
+}
+
+headerBurger.addEventListener('click', menuToggle);
